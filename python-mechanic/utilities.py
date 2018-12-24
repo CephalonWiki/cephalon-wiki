5bc9af58502fae471ddb4aeadebc9793b7b439cc
Cephalon Wiki <43304356+CephalonWiki@users.noreply.github.com>
2018-12-24 13:28:05 -0500

    Creating new utilities for testing, development - general purpose scraping functions

---
+++ python-mechanic/utilities.py
@@ -0,0 +1,13 @@
+import requests
+import json
+from lxml import html
+
+def search(title):
+    suggestions_json = requests.get("http://warframe.wikia.com/api/v1/SearchSuggestions/List?query=" + title.replace(" ", "_"))
+    suggestions_dict = [d["title"] for d in json.loads(suggestions_json.content.decode('utf-8'))["items"]]
+    return suggestions_dict
+
+def article(title):
+    article_main = requests.get("https://warframe.fandom.com/wiki/" + title.replace(' ','_').replace("&", "%26").replace(" and ", " %26 "))
+    article_tree = html.fromstring(article_main.content)
+    return article_tree
