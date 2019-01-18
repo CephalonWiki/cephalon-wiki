ad70c9563383fac99e7b0dd42c7f8ef1c5834463
Cephalon Wiki <43304356+CephalonWiki@users.noreply.github.com>
2019-01-17 23:08:58 -0500

    Captain Vor!!!!!

--- python-mechanic/test.py
+++ python-mechanic/test.py
@@ -4,4 +4,5 @@ sys.path.append('../python-bot')
 import RedditBotCephalonWiki
 
 test_bot = RedditBotCephalonWiki.RedditBotCephalonWiki(name = "test-cephalon-wiki", subreddit = "cephalonwiki")
+
 test_bot.scan()
