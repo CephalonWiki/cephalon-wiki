0bf815f469aa1fc8b4db1b12c1851f65fc346eb3
Cephalon Wiki <t_cubed@icloud.com>
2019-03-16 18:24:10 -0400

    Small tweak to recovery script

--- python-mechanic/recovery.py
+++ python-mechanic/recovery.py
@@ -20,10 +20,11 @@ for posts in post_list:
     for submission in posts:
         submission.comments.replace_more(limit=0)
         comments += list(filter(recovery_bot.should_respond, submission.comments.list()))
-        recovery_bot.logger.info("Gathered comments from submission \"" + submission.title + "\"")
+        recovery_bot.logger.debug("Gathered comments from submission \"" + submission.title + "\"")
 
 if comments:
-    recovery_bot.logger.info("Found the following comments:  %s", comments)
-    recovery_bot.scan(comments)
+    # using set as duplicate comments found in queue?
+    recovery_bot.logger.info("Found the following comments:  %s", set(comments))
+    recovery_bot.scan(set(comments))
 else:
-    recovery_bot.logger.warning("No comments found.  Terminating.")
+    recovery_bot.logger.warning("No comments found during recovery.  Terminating.")
