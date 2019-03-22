0e289856aaea61fa9e14b940ebd15b6fe5525044
Cephalon Wiki <43304356+CephalonWiki@users.noreply.github.com>
2019-03-22 00:19:41 -0400

    Update RedditBotCephalonWiki.py

--- python-bot/RedditBotCephalonWiki.py
+++ python-bot/RedditBotCephalonWiki.py
@@ -46,34 +46,37 @@ class RedditBotCephalonWiki(RedditBot.RedditBot):
 
         # Prepare 4 criteria:  Match brackets, human author, no previous reply, not on blacklist
         matches_brackets = ("{" in comment.body and comment.body.find("}", comment.body.rfind("{")) > 0)
-        human_author = comment.author != "CephalonWiki"
-
-        # Given updates to reply author code, blacklist should not be necessary
-        no_blacklist = str(comment) not in ['e7fnpxb', 'e5d8sl2']
-
-        # refresh comment before retrieving replies
-        comment.refresh()
-        comment.replies.replace_more(limit = 0)
-        reply_authors = list(map(lambda c: c.author, comment.replies.list()))
-        no_reply = "CephalonWiki" not in reply_authors
-
         if not matches_brackets:
             # most comments will not invoke the bot, so take no action
             return False
-        elif not human_author:
-            self.logger.warning("Comment authored by CephalonWiki")
-            return False
-        elif not no_reply:
-            self.logger.warning("Already replied to comment %s", comment)
-            return False
-        elif not no_blacklist:
-            self.logger.exception("Attempted to reply to blacklisted comment %s", comment)
-            return False
         else:
-            # Comment meets minimum criteria.
-            self.logger.debug("Should respond to comment %s", comment)
-            self.logger.info("***** NEW COMMENT *****")
-            return True
+            # Now that the comment is likely a candidate, do more processing
+            human_author = comment.author != "CephalonWiki"
+
+            # Given updates to reply author code, blacklist should not be necessary
+            no_blacklist = str(comment) not in ['e7fnpxb', 'e5d8sl2']
+
+            if not human_author:
+                self.logger.warning("Comment authored by CephalonWiki")
+                return False
+            elif not no_blacklist:
+                self.logger.exception("Attempted to reply to blacklisted comment %s", comment)
+                return False
+            else:
+                # Now that the comment is human-authored and contains matching braces...
+                # ...refresh comment and check replies
+                comment.refresh()
+                comment.replies.replace_more(limit=0)
+                reply_authors = list(map(lambda c: c.author, comment.replies.list()))
+                no_reply = "CephalonWiki" not in reply_authors
+                if not no_reply:
+                    self.logger.warning("Already replied to comment %s", comment)
+                    return False
+                else:
+                    # Comment meets minimum criteria.
+                    self.logger.debug("Should respond to comment %s", comment)
+                    self.logger.info("***** NEW COMMENT *****")
+                    return True
 
 
     # prepare summary of article, as a string
